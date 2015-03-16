fdf752247f210c5bdd838baacf645fa37cac5564
zethra <jediben97@gmail.com>
2015-03-16 17:44:48 -0400

    App done .. for now.

--- app/src/main/java/com/team3637/exterminate/MainActivity.java
+++ app/src/main/java/com/team3637/exterminate/MainActivity.java
@@ -1,9 +1,11 @@
 package com.team3637.exterminate;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,23 +19,29 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.exterminate);
+                mediaPlayer.start();
+                while (mediaPlayer.isPlaying()){}
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
+        }).start();
     }
 }
